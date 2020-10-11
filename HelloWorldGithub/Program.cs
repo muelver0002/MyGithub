796ac5547ac9b610184366856d74adc8f845eed1
muelver0002 <72661100+muelver0002@users.noreply.github.com>
2020-10-11 09:01:30 +0800

    added Hello Csharp program

---
+++ HelloWorldGithub/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace HelloWorldGithub
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
